d3f675f0f64fd5e50a587cd69b0ea927fd317b37
Mitry Burmistrov <mitekxxx@gmail.com>
2013-04-09 01:27:59 +0400

    Part 6 finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Product.delete_all
+#. . .
+Product.create(title: 'Ford Focus',
+  description:
+    %{<p>
+      Ford Focus is a crappy car for middle class.
+    </p>},
+    image_url: 'focus.jpg',
+    price: 23000.00)
+#. . .
+Product.create(title: 'Seat Leon',
+  description:
+    %{<p>
+      Seat Leon is the best car below $100,000.
+    </p>},
+    image_url: 'rails.png',
+    price: 20000.00)
+#. . .
+Product.create(title: 'VW Golf',
+  description:
+    %{<p>
+      VW Golf is the most legendary car. The golf class is named after this car.
+    </p>},
+    image_url: 'golf.jpg',
+    price: 25000.00)
+#. . .
